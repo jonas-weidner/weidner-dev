adc43a6a3965985d328a58d9a2d84be1803f4c69
Jonas Weidner <jonas@deity.io>
2023-05-01 16:45:02 +0200

    feat: Mum 60

---
+++ pages/mum-is-turning-60.tsx
@@ -0,0 +1,11 @@
+import { FC, useEffect } from "react";
+
+const MumTurns60: FC = () => {
+  useEffect(() => {
+    window.open('https://www.pictramap.com/app/view?p=ab7cf3&ui=2', '_self');
+  }, [])
+
+  return <></>;
+}
+
+export default MumTurns60;
